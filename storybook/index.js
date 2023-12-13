6a92101afdee1ae53d01d1f2f84739999f853a77
flochtililoch <flochtililoch@gmail.com>
2023-12-13 12:25:59 -0800

    chore: add bug culprit lines

--- storybook/index.js
+++ storybook/index.js
@@ -1,4 +1,6 @@
 import {AppRegistry} from 'react-native';
 import Storybook from './storybook';
+import {configure} from '@storybook/react-native';
+configure(() => {}, module);
 
 AppRegistry.registerComponent('sbbug', () => Storybook);

c36b5e1eda0ff01229cf1d7d3e461d2d14da954f
flochtililoch <flochtililoch@gmail.com>
2023-12-12 18:20:25 -0800

    chore: create two dummy components first uses storyOf, other uses CSF

---
+++ src/components/hello/stories.js
@@ -0,0 +1,13 @@
+import Hello from '.';
+
+const argTypes = {};
+
+export default {
+  component: Hello,
+  title: 'Components/Hello',
+};
+
+export const Default = {
+  args: {},
+  argTypes,
+};
---
+++ src/components/world/stories.js
@@ -0,0 +1,7 @@
+// @flow
+
+import World from '.';
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+
+storiesOf('Components/World', module).add('Default', () => <World />);
